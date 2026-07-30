f397e20db789d0532c9f302388c0b22f4eacc47a
Galf-eng <61783583+barumank@users.noreply.github.com>
2026-07-31 00:46:04 +0300

    Add files via upload

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,16 @@
+import { Sidebar } from '@/src/components/layout/sidebar';
+import { Header } from '@/src/components/layout/header';
+
+export default function MainLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen bg-[#0D0D1A]">
+      <Sidebar />
+      <div className="ml-[240px] flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-1 p-6">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
